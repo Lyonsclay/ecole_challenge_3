560ffe966bbadf38ba37e99c6de3f0986bd9a42d
Lyonsclay <lyonsclay@yahoo.com>
2016-03-12 13:17:48 -0500

    Refactor and apply jslint.

--- challenge.js
+++ challenge.js
@@ -1,34 +1,47 @@
+var itemForm = document.querySelector('#itemForm');
+var ul = document.querySelector('#list');
+
+function addButton(button) {
+    "use strict";
+
+    button.innerHTML = "Remove";
+
+    button.addEventListener('click', function () {
+        button.parentNode.remove();
+    });
+}
+
+function addListNode(input, button) {
+    "use strict";
+
+    var li = document.createElement('li');
+
+    li.innerHTML = input;
+    li.appendChild(button);
+    ul.appendChild(li);
+}
+
+function addToList() {
+    "use strict";
+
+    var itemInput = itemForm.querySelector('#itemInput');
+    var input = itemInput.value;
+    var button = document.createElement('button');
+
+    addButton(button);
+
+    addListNode(input, button);
+
+    itemInput.value = '';
+}
+
 document.onreadystatechange = function () {
-    
+    "use strict";
+
     if (document.readyState === 'complete') {
-        var list = [],
-            listForm = document.querySelector('#addItem'),
-            ul = document.querySelector('#list'),
-            input = listForm.querySelector('input');
-
-
-        addItem.addEventListener('submit', function (event) {
+        itemForm.addEventListener('submit', function () {
             event.preventDefault();
             addToList();
         });
-
-        var addToList = function () {
-            var item = addItem.querySelector('input').value,
-                newItem = listForm.querySelector.innerHTML,
-                li = document.createElement('li'),
-                button = document.createElement('button');
-
-            button.innerHTML = "Remove";
-            input.value = '';
-
-            button.addEventListener('click', function(event) {
-                this.parentNode.remove();
-            })
-
-            list.push(item);
-            li.innerHTML = item;
-            li.appendChild(button);
-            ul.appendChild(li);
-        };
     }
-}
+};
